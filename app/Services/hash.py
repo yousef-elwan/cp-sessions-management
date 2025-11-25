cefcd1b6b5618adf8ec89f42f08c7bb62ff145cd
mohamad-shaban <mohamadshaban2001@gmail.com>
2025-11-25 15:46:32 +0300

    solve hash problem

--- app/Services/hash.py
+++ app/Services/hash.py
@@ -1,11 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    return password
-    truncated_password = password.encode('utf-8')[:72]
-    return pwd_context.hash(truncated_password)
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
